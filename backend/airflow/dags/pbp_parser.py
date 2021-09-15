51883f35cd002c1b423356e1f893a67e333f2684
Sam Pochyly <sam.pochyly@gmail.com>
2021-09-14 22:59:12 -0500

    cleaned up parsing code (comments/naming) and added kickoff parsers

--- backend/airflow/dags/pbp_parser.py
+++ backend/airflow/dags/pbp_parser.py
@@ -1,25 +1,80 @@
 import re
 import constants as c
 
+def wrap_expressions(expressions: dict) -> dict:
+    """
+    Given a dictionary mapping variable names to regex patterns,
+    returns a new dictionary mapping the same variable name to a new regex 
+    expression that will return the variable name in the regex .groupdict() function
 
-def regex_wrapper(regex_dict):
-    return {var: r"(?P<%s>%s)" % (var, regex) for (var, regex) in regex_dict.items()}
+    New pattern of the form (?P<variable_name>{regex_expr})
 
-def run_play(string):
+    This allows the formation of cleaner and more readable regex compositions 
 
-    regex_components = {
+    Example: 
+    **Note, I've used [d] instead of the proper regex to avoid an invalid escape sequence warning
+
+        Input: {"year": r"[d]{4}"}
+        Output: {"year": r"(P?<year>[d]{4})"}
+
+    Example Usage:
+        >>> regex_components = {"year": r"[d]{4}", "month": r"[d]{2}"}
+        >>> pattern = r"%(year)s %(month)s" % wrap_expressions(regex_components)
+        >>> re.match(pattern, "2021 01").groupdict()
+        {"year": "2021", "month": "01"}
+    """
+    return {var: r"(?P<%s>%s)" % (var, regex) for (var, regex) in expressions.items()}
+
+
+def parse_run_play(play_description: str) -> dict or None:
+    """
+    Given a play by play description, if it's a RUN play,
+    returns the regex match dictionary for each piece of information in the description
+    Otherwise, returns None
+
+    RUN plays should be of the form:
+        {Player Name} {direction} for {distance} [(tackle by {tackler})]
+
+    Example: 
+        play_description = "David Montgomery right tackle for 15 yards
+        returns: {
+            "runner": "David Montgomery", 
+            "direction": "right tackle", 
+            "distance": "15 yards", 
+            "tackler": None
+        }
+    """
+
+    expressions = {
         'runner': c.PLAYER,
         'direction': r"|".join(c.RUN_DIRECTIONS),
         'distance': r"|".join(c.DISTANCES),
         'tackler': c.PLAYER
     }
 
-    pattern = r"%(runner)s %(direction)s for %(distance)s( \(tackle by %(tackler)s\))?" % regex_wrapper(regex_components)
-    return re.match(pattern, string)
+    pattern = r"%(runner)s %(direction)s for %(distance)s( \(tackle by %(tackler)s\))?" % wrap_expressions(expressions)
+    return re.match(pattern, play_description)
 
 
-def complete_pass(string):
-    regex_components = {
+def parse_completed_pass_play(play_description: str) -> dict or None:
+    """
+    Given a play by play description, if it's a COMPLETED PASS play,
+    returns the regex match dictionary for each piece of information in the description
+    Otherwise, returns None
+
+    COMPLETED PASS plays should be of the form:
+        {Player Name} pass complete {direction} to {reciever} for {distance} [(tackle by {tackler})]
+
+    Example: 
+        play_description = "Justin Fields pass complete deep right to Allen Robinson for 45 yards (tackle by Jalen Ramsey)"
+        returns: {
+            "runner": "Justin Fields", 
+            "direction": "deep right", 
+            "distance": "45 yards", 
+            "tackler": Jalen Ramsey
+        }
+    """
+    expressions = {
         'passer': c.PLAYER,
         'direction': r"|".join(c.PASS_DIRECTIONS),
         'reciever': c.PLAYER,
@@ -27,19 +82,94 @@ def complete_pass(string):
         'tackler': c.PLAYER
     }
 
-    pattern = r"%(passer)s pass complete %(direction)s to %(reciever)s for %(distance)s( \(tackle by %(tackler)s\))?" % regex_wrapper(regex_components)
+    pattern = r"%(passer)s pass complete %(direction)s to %(reciever)s for %(distance)s( \(tackle by %(tackler)s\))?" % wrap_expressions(expressions)
 
-    return re.match(pattern, string)
+    return re.match(pattern, play_description)
 
 
-def incomplete_pass(string):
-    regex_components = {
+def parse_incomplete_pass_play(play_description: str) -> dict or None:
+    """
+    Given a play by play description, if it's a INCOMPLETE PASS play,
+    returns the regex match dictionary for each piece of information in the description
+    Otherwise, returns None
+
+    COMPLETED PASS plays should be of the form:
+        {Player Name} pass complete {direction} to {reciever} for {distance} [(tackle by {tackler})]
+
+    Example: 
+        play_description = "Justin Fields pass complete deep right to Allen Robinson for 45 yards (tackle by Jalen Ramsey)"
+        returns: {
+            "runner": "Justin Fields", 
+            "direction": "deep right", 
+            "distance": "45 yards", 
+            "tackler": "Jalen Ramsey"
+        }
+    """
+    expressions = {
         'passer': c.PLAYER,
         'direction': r"|".join(c.PASS_DIRECTIONS),
         'reciever': c.PLAYER,
         'defender': c.PLAYER
     }
 
-    pattern = r"%(passer)s pass incomplete %(direction)s intended for %(reciever)s( \(defended by %(defender)s\))?" % regex_wrapper(regex_components)
+    pattern = r"%(passer)s pass incomplete %(direction)s intended for %(reciever)s( \(defended by %(defender)s\))?" % wrap_expressions(expressions)
 
-    return re.match(pattern, string)
+    return re.match(pattern, play_description)
+
+
+def parse_kickoff_touchback(play_description: str) -> dict or None:
+    """
+    Given a play by play description, if it's a KICKOFF TOUCHBACK,
+    returns the regex match dictionary for each piece of information in the description
+    Otherwise, returns None
+
+    KICKOFF TOUCHBACKS should be of the form:
+        {Player Name} kicks off {distance}, touchback.
+
+    Example: 
+        play_description = "Robbie Gould kicks off 65 yards, touchback"
+        returns: {
+            "kicker": "Robbie Gould", 
+            "kick_distance": "65 yards"
+        }
+    """
+    expressions = {
+        'kicker': c.PLAYER,
+        'distance': r"|".join(c.DISTANCES)
+    }
+
+    pattern = r"%(kicker)s kicks off %(distance)s, touchback" % wrap_expressions(expressions)
+
+    return re.match(pattern, play_description)
+
+
+def parse_kickoff_returned(play_description: str) -> dict or None:
+    """
+    Given a play by play description, if it's a KICKOFF RETURNED,
+    returns the regex match dictionary for each piece of information in the description
+    Otherwise, returns None
+
+    KICKOFF RETURNED should be of the form:
+        {Player Name} kicks off {distance}, returned by {Player Name} for {distance} [(tackle by {tackler})]
+
+    Example: 
+        play_description = "Robbie Gould kicks off 73 yards, returned by Cordarrelle Patterson for 54 yards"
+        returns: {
+            "kicker": "Robbie Gould", 
+            "kick_distance": "73 yards", 
+            "returner": "Cordarrelle Patterson",
+            "return_distance": "54 yards",
+            "tackler": None
+        }
+    """
+    expressions = {
+        'kicker': c.PLAYER,
+        'kick_distance': r"|".join(c.DISTANCES),
+        'returner': c.PLAYER,
+        'return_distance': r"|".join(c.DISTANCES),
+        'tackler': c.PLAYER
+    }
+
+    pattern = r"%(kicker)s kicks off %(kick_distance)s, returned by %(returner)s for %(return_distance)s( \(tackle by %(tackler)s\))?" % wrap_expressions(expressions)
+
+    return re.match(pattern, play_description)
--- backend/airflow/dags/test_parsing.py
+++ backend/airflow/dags/test_parsing.py
@@ -1,10 +1,17 @@
 import re
 import constants as c
 import pbp_parser 
+from typing import List
 
-players = ['First Last', 'First Middle Last', 'First Last-Last', 'First L"Last', "First L'Last"]
+PLAYER_EXAMPLES = [
+    'First Last', 
+    'First Middle Last', 
+    'First Last-Last', 
+    'First L"Last', 
+    "First L'Last"
+]
 
-distances = [
+DISTANCE_EXAMPLES = [
     'no gain',
     '1 yard',
     '2 yards', 
@@ -13,83 +20,209 @@ distances = [
     '20 yards'
 ]
 
-tacklers = [
-    '',
-    *[f'(tackle by {player})' for player in players],
-    *[f'(tackle by {player} and {player})' for player in players]
+TACKLER_EXAMPLES = [
+    '', # no tackler
+    *[f'(tackle by {player})' for player in PLAYER_EXAMPLES],
+    *[f'(tackle by {player} and {player})' for player in PLAYER_EXAMPLES]
 ]
 
-defenders = [
-    '', 
-    *[f'(defended by {player}' for player in players]
+DEFENDER_EXAMPLES = [
+    '', # no defender
+    *[f'(defended by {player}' for player in PLAYER_EXAMPLES]
 ]
 
-touchdowns = [
-    '',
+TOUCHDOWN_EXAMPLES = [
+    '', # no touchdown
     ', touchdown'
 ]
 
-def test_regex_wrapper():
-    assert pbp_parser.regex_wrapper({'key1': r"value"}) == {'key1': r"(?P<key1>value)"}
+def get_example_run_plays() -> List[str]:
+    """
+    Gets a list of example RUN plays by iterating through different 
+    example permutations
 
-    examples = {
-        'key1': r"value",
-        'key2': r"\d+",
-        'key3': r"[+-]?\d+\.\d\d"
+    Ex: David Montgomery right tackle for 10 yards, touchdown 
+    """
+    return [
+        f'{player} {direction} for {distance}{tackler}'
+        for player in PLAYER_EXAMPLES
+        for direction in c.RUN_DIRECTIONS
+        for distance in DISTANCE_EXAMPLES
+        for tackler in TACKLER_EXAMPLES
+    ]
+
+
+def get_example_completed_pass_plays() -> List[str]:
+    """
+    Gets a list of example COMPLETED PASS plays by iterating through different 
+    example permutations
+
+    Ex: Justin Fields pass complete deep right to Allen Robinson for 45 yards, touchdown
+    """
+    return [
+        f'{player} pass complete {direction} to {player} for {distance}{tackler}{touchdown}'
+        for player in PLAYER_EXAMPLES
+        for direction in c.PASS_DIRECTIONS
+        for distance in DISTANCE_EXAMPLES
+        for tackler in TACKLER_EXAMPLES
+        for touchdown in TOUCHDOWN_EXAMPLES
+    ]
+
+
+def get_example_incomplete_pass_plays() -> List[str]:
+    """
+    Gets a list of example INCOMPLETE PASS plays by iterating through different 
+    example permutations
+
+    Ex: Aaron Rodgers pass incomplete short right intended for Davante Adams, defended by Jaylon Johnson
+    """
+    return [
+        f'{player} pass incomplete {direction} intended for {player}{defender}'
+        for player in PLAYER_EXAMPLES
+        for direction in c.PASS_DIRECTIONS
+        for defender in DEFENDER_EXAMPLES
+    ]
+
+
+def get_example_kickoff_touchback_plays() -> List[str]:
+    """
+    Gets a list of example KICKOFF TOUCHBACK plays 
+
+    Ex: Robbie Gould kicks off 65 yards, touchback
+    """
+    return [
+        f'{player} kicks off {distance}, touchback'
+        for player in PLAYER_EXAMPLES
+        for distance in DISTANCE_EXAMPLES
+    ]
+
+
+def get_example_kickoff_returned_plays() -> List[str]:
+    """
+    Gets a list of example KICKOFF RETURNED plays 
+
+    Ex: Robbie Gould kicks off 65 yards, returned by returned by Cordarrelle Patterson for 54 yards
+    """
+    return [
+        f'{kicker} kicks off {kick_distance}, returned by {returner} for {return_distance}{tackler}'
+        for kicker in PLAYER_EXAMPLES
+        for kick_distance in DISTANCE_EXAMPLES
+        for returner in PLAYER_EXAMPLES
+        for return_distance in DISTANCE_EXAMPLES
+        for tackler in TACKLER_EXAMPLES
+    ]
+
+
+def get_example_plays_except(excluded_play_type: str) -> List[str]:
+    """
+    Get's a list of all the play types, except the one specified as a parameter
+    This is for checking that the regex does not match
+    """
+    assert excluded_play_type in ['run', 'completed_pass', 'incomplete_pass', 'kickoff_touchback', 'kickoff_returned'], \
+        f'Invalid play type: {excluded_play_type}'
+
+    all_play_types = []
+    if excluded_play_type != 'run':
+        all_play_types += get_example_run_plays()
+
+    if excluded_play_type != 'completed_pass':
+        all_play_types += get_example_completed_pass_plays()
+
+    if excluded_play_type != 'incomplete_pass':
+        all_play_types += get_example_incomplete_pass_plays()
+
+    if excluded_play_type != 'kickoff_touchback':
+        all_play_types += get_example_kickoff_touchback_plays()
+
+    if excluded_play_type != 'kickoff_returned':
+        all_play_types += get_example_kickoff_returned_plays()
+
+    return all_play_types
+
+
+def test_wrap_expressions():
+    """
+    Tests the wrapper to help match variables to regex patterns
+    """
+    expressions = {
+        'year': r"[\d]{4}",
+        'month': r"[\d]{2}"
     }
-    test_string = 'value 12345 +30.02'
-    test_regex = r"%(key1)s %(key2)s %(key3)s" % pbp_parser.regex_wrapper(examples)
+    wrapped_expressions = {
+        'year': r"(?P<year>[\d]{4})",
+        'month': r"(?P<month>[\d]{2})"
+    }
+    assert pbp_parser.wrap_expressions(expressions) == wrapped_expressions
+
+    test_string = '2019 01'
+    test_regex = r"%(year)s %(month)s" % pbp_parser.wrap_expressions(expressions)
     expected_matching = {
-        'key1': 'value',
-        'key2': '12345',
-        'key3': '+30.02'
+        'year': '2019',
+        'month': '01'
     }
     assert re.match(test_regex, test_string).groupdict() == expected_matching
 
-def get_run_plays():
-    return [
-        f'{player} {direction} for {distance}{tackler}'
-        for player in players
-        for direction in c.RUN_DIRECTIONS
-        for distance in distances
-        for tackler in tacklers
-    ]
-
-def get_complete_pass_plays():
-    return [
-        f'{player} pass complete {direction} to {player} for {distance}{tackler}{touchdown}'
-        for player in players
-        for direction in c.PASS_DIRECTIONS
-        for distance in distances
-        for tackler in tacklers
-        for touchdown in touchdowns
-    ]
-
-def get_incomplete_pass_plays():
-    return [
-        f'{player} pass incomplete {direction} intended for {player}{defender}'
-        for player in players
-        for direction in c.PASS_DIRECTIONS
-        for defender in defenders
-    ]
 
 def test_run_parsing():
-    for test_string in get_run_plays():
-        assert pbp_parser.run_play(test_string), f'No match with run play regex: {test_string}'
+    """
+    Test that the RUN play parser matches only run plays
+    """
+    for test_string in get_example_run_plays():
+        assert pbp_parser.parse_run_play(test_string), \
+            f'No match with run play regex: {test_string}'
 
-    for test_string in get_complete_pass_plays() + get_incomplete_pass_plays():
-        assert pbp_parser.run_play(test_string) is None, f'Test string not a run play but matched regex: "{test_string}"'
+    for test_string in get_example_plays_except('run'):
+        assert pbp_parser.parse_run_play(test_string) is None, \
+            f'Test string not a run play but matched regex: "{test_string}"'
 
-def test_complete_pass_parsing():
-    for test_string in get_complete_pass_plays():
-        assert pbp_parser.complete_pass(test_string), f'No match with completed pass regex: "{test_string}"'
+
+def test_completed_pass_parsing():
+    """
+    Test that the COMPLETED PASS play parser matches only completed pass plays
+    """
+    for test_string in get_example_completed_pass_plays():
+        assert pbp_parser.parse_completed_pass_play(test_string), \
+            f'No match with completed pass regex: "{test_string}"'
     
-    for test_string in get_run_plays() + get_incomplete_pass_plays():
-        assert pbp_parser.complete_pass(test_string) is None, f'Test string not a completed pass play but matched regex: "{test_string}"'
+    for test_string in get_example_plays_except('completed_pass'):
+        assert pbp_parser.parse_completed_pass_play(test_string) is None, \
+            f'Test string not a completed pass play but matched regex: "{test_string}"'
+
 
 def test_incomplete_pass_parsing():
-    for test_string in get_incomplete_pass_plays():
-        assert pbp_parser.incomplete_pass(test_string), f'No match with incomplete pass regex: "{test_string}"'
+    """
+    Test that the INCOMPLETE PASS play parser matches only incomplete pass plays
+    """
+    for test_string in get_example_incomplete_pass_plays():
+        assert pbp_parser.parse_incomplete_pass_play(test_string), \
+            f'No match with incomplete pass regex: "{test_string}"'
     
-    for test_string in get_run_plays() + get_complete_pass_plays():
-        assert pbp_parser.incomplete_pass(test_string) is None, f'Test string not an incompleted pass play but matched regex: "{test_string}"'
+    for test_string in get_example_plays_except('incomplete_pass'):
+        assert pbp_parser.parse_incomplete_pass_play(test_string) is None, \
+            f'Test string not an incompleted pass play but matched regex: "{test_string}"'
+
+
+def test_kickoff_touchback_parsing():
+    """
+    Test that the KICKOFF TOUCHBACK play parser matches kickoff touchbacks
+    """
+    for test_string in get_example_kickoff_touchback_plays():
+        assert pbp_parser.parse_kickoff_touchback(test_string), \
+            f'No match with kickoff touchback regex: "{test_string}"'
+    
+    for test_string in get_example_plays_except('kickoff_touchback'):
+        assert pbp_parser.parse_kickoff_touchback(test_string) is None, \
+            f'Test string not a kickoff touchback but matched regex: "{test_string}"'
+
+
+def test_kickoff_returned_parsing():
+    """
+    Test that the KICKOFF RETURNED play parser matches only kickoffs returned
+    """
+    for test_string in get_example_kickoff_returned_plays():
+        assert pbp_parser.parse_kickoff_returned(test_string), \
+            f'No match with kickoff touchback regex: "{test_string}"'
+    
+    for test_string in get_example_plays_except('kickoff_returned'):
+        assert pbp_parser.parse_kickoff_returned(test_string) is None, \
+            f'Test string not a kickoff touchback but matched regex: "{test_string}"'
